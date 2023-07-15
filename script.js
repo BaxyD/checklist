3a743d656f2640fa768d3b6b659bc9b1966846d3
BaxyD <112361061+BaxyD@users.noreply.github.com>
2023-07-14 21:20:13 -0500

    updating js and css

--- script.js
+++ script.js
@@ -3,8 +3,17 @@ document.getElementById('myInput').addEventListener('keydown', function(event) {
         event.preventDefault(); // prevent default form submission
         let inputValue = this.value; // get the input value
         let listItem = document.createElement('li'); // create a new list item
+        listItem.setAttribute('type', 'checkbox'); // set type to checkbox
+        listItem.id = `checkbox-${listItem.id}`; // sets id for new list items
         listItem.textContent = inputValue; // set the content of the list item
         document.getElementById('myList').appendChild(listItem); // append the list item to the unordered list
+        
+        listItem.addEventListener('click', function(check){
+            this.classList.toggle('crossed-out');
+        }); // adding a crossed out function nested in the original function
+        
         this.value = ''; // clear the input field
     }
 })
+
+
